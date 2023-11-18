f45915956d635b769f9082548cd9210d0d25100c
Filipusmeitriboygulo <filipusmeitriboygulo@gmail.com>
2023-11-19 00:07:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.1/dist/css/bootstrap.min.css">
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    
+</head>
+<body id="home">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary  shadow-sm mb-2">
+        <div class="container">
+          <a class="navbar-brand" href="#">Filipus Mei TBG</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+              <a class="nav-link active" aria-current="page" href="home">Home</a>
+              <a class="nav-link" href="About">About Me</a>
+              <a class="nav-link" href="Projects">Projects</a>
+              <a class="nav-link" href="Contact">Contact Me</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    <!-- Akhir Navbar -->
+    <!-- Jumbotron -->
+    <section class="jumbotron text-dark text-center">
+      <img src="img/pp (2).jpg" alt="Filipus Mei TBG" width="200" height="200" class="rounded-circle img-thumbnail" >
+      <h1 class="display-4 ">Filipus Mei Tri Boy Gulo</h1>
+      <p class="lead">Mahasiswa | Freelancer</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,218.7C384,192,480,128,576,85.3C672,43,768,21,864,26.7C960,32,1056,64,1152,69.3C1248,75,1344,53,1392,42.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,
+      672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      </section>
+    <!-- Akhir Jumbotron -->
+    <!-- About -->
+    <section id="About">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center mb-3 ">
+          <div class="col-md-4">
+            <p>Saya Merupakan Mahasiswa Teknik Informatika di Politeknik Negeri Lhokseumawe 
+            dan <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-balloon-heart" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="m8 2.42-.717-.737c-1.13-1.161-3.243-.777-4.01.72-.35.685-.451 1.707.236 3.062C4.16 6.753 5.52 8.32 8 10.042c2.479-1.723 3.839-3.29 4.491-4.577.687-1.355.587-2.377.236-3.061-.767-1.498-2.88-1.882-4.01-.721L8 2.42Zm-.49 8.5c-10.78-7.44-3-13.155.359-10.063.045.041.089.084.132.129.043-.045.087-.088.132-.129 3.36-3.092 11.137 2.624.357 10.063l.235.468a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.235-.468ZM6.013 2.06c-.649-.18-1.483.083-1.85.798-.131.258-.245.689-.08 1.335.063.244.414.198.487-.043.21-.697.627-1.447 1.359-1.692.217-.073.304-.337.084-.398Z"/>
+            </svg></p>
+          </div>
+          <div class="col-md-4">
+            <p>
+            Selain itu saya juuga membuka jasa untuk membuat layannan website anda, ataupun aplikasi berbasis android maupun dekstop
+            portofolio saya menjadi pertimbangan anda
+            </p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill=" #e2edff" fill-opacity="1" d="M0,64L48,90.7C96,
+        117,192,171,288,181.3C384,192,480,160,576,176C672,192,768,256,864,282.7C960,309,1056,299,1152,272C1248,245,1344,203,1392,181.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,
+        320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    <!-- Akhir About -->
+    <!-- Content/project -->
+    <section id="Projects">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <h2>My Projects</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/Screenshot (99).png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Project 1 | Cafe Boy.com</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/Screenshot (101).png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Project 2 | Kelas Ngoding.co.edu</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="img/Screenshot (102).png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Project 3 | Telusur.edu</p>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,32L48,32C96,32,192,32,288,69.3C384,107,480,181,576,197.3C672,213,768,171,864,138.7C960,107,1056,85,1152,85.3C1248,85,1344,107,1392,117.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,
+      320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    <!-- Akhir Content/project -->
+    <!-- Contact Me -->
+    <section id="Contact">
+      <div class="container text-left ">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>Contact Me </h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+          <form>
+            <div class="mb-3">
+              <label for="name" class="form-label">Nama Lengkap :</label>
+              <input type="text" class="form-control" id="name" aria-describedby="name">
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email :</label>
+              <input type="email" class="form-control" id="email" aria-describedby="email">
+            </div>
+            <div class="mb-3">
+            <label for="pesan" class="form-label">Pesan</label>
+            <textarea class="form-control" id="pesan" rows="3"></textarea>
+            </div>
+            <div class="col">
+              <button type="submit" class="btn btn-primary mb-3">Kirim</button>
+            </div>
+          </form>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,96L48,128C96,160,192,224,288,234.7C384,245,480,203,576,160C672,117,768,75,864,85.3C960,96,1056,160,1152,160C1248,160,1344,96,1392,64L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,
+      320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section> 
+    <!-- Akhir Contact Me -->
+  <!-- Footer -->
+  <footer class="bg-primary text-white text-center ">
+    <p>Created By <a href="https://www.youtube.com/channel/UCSZg33gEgEgzRgcdNRMDR9Q" class="text-white fw-bold">Filipus MTB Gulo</a></p>
+  </footer>
+  <!-- Akhir Footer -->
+    <script src="bootstrap-5.2.1/dist/js/bootstrap.bundle.js"></script>
+</body>
+</html>
